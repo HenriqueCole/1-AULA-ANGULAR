827b322ba20e40bcab8489434cfa260d432ff88c
HenriqueCole <henrique_c_fernandes@estudante.sesisenai.org.br>
2021-12-08 21:40:27 -0300

    Upload

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  constructor() { }
+  name = "";
+  surname = "";
+  birthDate = undefined;
+  listPersons = [];
+  ngOnInit() {
+  }
+
+
+  registerPerson() {
+    let person = {
+      name: this.name,
+      surname: this.surname,
+      birthDate: this.birthDate
+    }
+
+    this.listPersons.push(person)
+
+    this.name = "";
+    this.surname = "";
+    this.birthDate = undefined;
+  }
+
+  selectPerson(personData, indexPerson){
+    this.name = personData.name;
+    this.surname = personData.surname;
+    this.birthDate = personData.birthDate;
+    this.listPersons.splice(indexPerson, 1)
+}
+
+  deletePerson(indexPerson){
+    this.listPersons[indexPerson].deleted = true;
+  }
+  
+  restorePerson(indexPerson){
+    delete this.listPersons[indexPerson].deleted;
+  }
+}
